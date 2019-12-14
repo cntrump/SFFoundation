f63d7cff82da17369b0d87928c9db7ab95ee8ab9
vvveiii <cntrump@gmail.com>
2019-12-14 09:29:16 +0800

    update SFURIFixup

--- SFFoundation/SFURL/SFURIFixup.m
+++ SFFoundation/SFURL/SFURIFixup.m
@@ -22,33 +22,6 @@ static NSString *replaceBrackets(NSString *url) {
     return [[url stringByReplacingOccurrencesOfString:@"[" withString:@"%5B"] stringByReplacingOccurrencesOfString:@"]" withString:@"%5D"];
 }
 
-static NSURL *reformatURL(NSURL *url) {
-    NSURLComponents *components = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:NO];
-    NSArray<NSURLQueryItem *> *queryItems = components.queryItems;
-    NSUInteger queryCount = queryItems.count;
-    if (queryCount > 0) {
-        NSMutableString *percentEncodedQuery = NSMutableString.string;
-        for (NSUInteger i = 0; i < queryCount; i++) {
-            if (i > 0) [percentEncodedQuery appendString:@"&"];
-
-            NSURLQueryItem *queryItem = queryItems[i];
-            NSString *name = [queryItem.name stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.sf_URLAllowedCharacterSet];
-            NSString *value = queryItem.value;
-            if (value) {
-                value = [queryItem.value stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.sf_URLAllowedCharacterSet];
-                [percentEncodedQuery appendFormat:@"%@=%@", name, value];
-            } else {
-                [percentEncodedQuery appendString:name];
-            }
-        }
-
-        components.query = nil;
-        components.percentEncodedQuery = percentEncodedQuery;
-    }
-
-    return components.URL;
-}
-
 @implementation NSCharacterSet (SFURL)
 
 + (instancetype)sf_URLAllowedCharacterSet {
@@ -66,7 +39,7 @@ static NSURL *reformatURL(NSURL *url) {
 
     NSURL *url = [NSURL URLWithString:entry];
     if (url) {
-        return reformatURL(url);
+        return url;
     }
 
     NSString *trimmed = [entry stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
@@ -78,7 +51,7 @@ static NSURL *reformatURL(NSURL *url) {
     escaped = replaceBrackets(escaped);
     url = punycodedURL(escaped);
     if (url.scheme) {
-        return reformatURL(url);
+        return url;
     }
 
     if ([trimmed rangeOfString:@"."].length == 0) {
@@ -94,7 +67,7 @@ static NSURL *reformatURL(NSURL *url) {
         return nil;
     }
 
-    return reformatURL(url);
+    return url;
 }
 
 @end
